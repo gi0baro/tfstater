9bc27846bb618605a355aafb585ad089f49f49b2
Giovanni Barillari <giovanni.barillari@gmail.com>
2021-11-18 18:26:54 +0100

    Add health endpoint

--- tfstater/views/__init__.py
+++ tfstater/views/__init__.py
@@ -4,4 +4,9 @@ views = app.module(__name__, "views")
 views.pipeline = [sessions, db.pipe, auth.pipe]
 
 
+@app.route(methods="get", output="bytes")
+async def _health():
+    return b""
+
+
 from . import accounts, main
